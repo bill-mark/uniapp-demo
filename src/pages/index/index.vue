04e9e8e07d3831756044bc086957ff332b2b7cc4
bill.mark <2286449127@qq.com>
2019-08-26 10:56:36 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="content">
+		<view class="uni-list">
+            <view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in news" 
+                  :key="index" @tap="openinfo">
+                <view class="uni-media-list">
+                    <image class="uni-media-list-logo" :src="item.author_avatar"></image>
+                    <view class="uni-media-list-body">
+                        <view class="uni-media-list-text-top">{{item.title}}</view>
+                        <view class="uni-media-list-text-bottom uni-ellipsis">{{item.created_at}}</view>
+                    </view>
+                </view>
+            </view>
+        </view> 
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				news:[]
+			}
+		},
+		mounted(){
+           
+		},
+		onLoad(){
+           uni.request({
+           	url:'https://unidemo.dcloud.net.cn/api/news',
+           	method:'GET',
+           	data:{},
+           	success:res =>{
+           		this.news = res.data
+           	},
+           	fail:()=>{},
+           	complete:()=>{}
+           })
+		}, 
+		methods: {
+            openinfo(){
+            	console.log('openinfo')
+            	uni.navigateTo({
+                  url:'../info/info',                
+            	})
+            }
+		}
+	}
+</script>
+
+<style>
+	.uni-media-list-body{
+		height: auto;
+	}
+	.uni-media-list-text-top{
+		line-height: 1.6em;
+	}
+</style>
