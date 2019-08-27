398b864bd72e11428d9086b546c187407929a477
bill.mark <2286449127@qq.com>
2019-08-27 17:53:42 +0800

    获取路由参数

--- src/pages/info/info.vue
+++ src/pages/info/info.vue
@@ -1,18 +1,29 @@
 <template>
 	<view>
-		
+		<view>{{title}}</view>
 	</view>
 </template>
 
 <script>
 	export default{
+		data(){
+            return {
+            	title:''
+            }
+		},
         onLoad(e){
-          console.log('info')
-          console.log(e)
+          uni.showLoading({
+          	title:"加载中. 888"
+          }),
           uni.request({
           	url:'https://unidemo.dcloud.net.cn/api/news/36kr/'+e.newsid,
           	method:'GET',
-          	data:{}
+          	data:{},
+          	success:res=>{
+          		//console.log(res)
+          		this.title = res.data.title
+          		uni.hideLoading()
+          	}
           })
         }
 	}
